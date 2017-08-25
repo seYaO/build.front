e1d6654795eb0990226d12168954017efa95722c
wl09720 <wl09720@ly.com>
2017-08-25 18:00:28 +0800

    .

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const Koa = require('koa');
 const views = require('koa-views');
 const static = require('koa-static');
 const router = require('koa-router')();
+// const io = require('socket.io')(80);
 const app = new Koa();
 const port = 8989;
 
 const staticPath = './modules';
 
-app.use(static(
-    path.resolve(staticPath)
-))
+// app.use(static(
+//     path.resolve(staticPath)
+// ))
 
-app.use(views(
-    path.resolve('./'), {
-        map: {
-            html: 'ejs'
-        },
-    }
-));
+// app.use(views(
+//     path.resolve('./'), {
+//         map: {
+//             html: 'ejs'
+//         },
+//     }
+// ));
 
 // logger
 app.use(async(ctx, next) => {
@@ -30,11 +31,20 @@ app.use(async(ctx, next) => {
 })
 
 
-router.get('/', async ctx => {
-    await ctx.render('index.html');
-});
+// router.get('/', async ctx => {
+//     await ctx.render('index.html');
+// });
+
+router.get('/', ctx => {
+
+})
 
 app.use(router.routes()).use(router.allowedMethods());
+
+const io = require('socket.io')(app);
+io.on('connection', socket => {
+    //
+})
 
 app.listen(port, () => {
     console.log(`application is start: http://localhost:${port}`);
