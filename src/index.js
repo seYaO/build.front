1d13a7f58f3818cf9912bd8ce19be26d73e2f303
wl09720 <wl09720@ly.com>
2018-05-30 13:46:09 +0800

    update

--- src/index.js
+++ src/index.js
@@ -18,10 +18,12 @@ const app = dva({
 app.use(createLoading());
 
 // 3. Model
-app.model(require('./models/app').default);
+app.model(require('./models/global').default);
 
 // 4. Router
 app.router(require('./router').default);
 
 // 5. Start
 app.start('#root');
+
+export default app._store; // eslint-disable-line
