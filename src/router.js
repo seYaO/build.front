6a17a4cf0067eb090f0aae53da34a1cfccfca2fb
wl09720 <wl09720@ly.com>
2018-05-30 17:49:40 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect, routerRedux } from 'dva/router'
-import dynamic from 'dva/dynamic'
-import { LocaleProvider } from 'antd'
-// import enUS from 'antd/lib/locale-provider/en_US'
+import React from 'react'
+import { routerRedux, Route, Switch } from 'dva/router'
+import { LocaleProvider, Spin } from 'antd'
 import zhCN from 'antd/lib/locale-provider/zh_CN'
-import App from 'routes/app'
+import dynamic from 'dva/dynamic'
+import { getRouterData } from './common/router'
+import Authorized from './utils/Authorized'
+import styles from './index.less'
 
-const { ConnectedRouter } = routerRedux
+const { ConnectedRouter } = routerRedux;
+const { AuthorizedRoute } = Authorized;
+dynamic.setDefaultLoadingComponent(() => {
+    return <Spin size="large" className={styles.globalSpin} />;
+});
 
 function RouterConfig({ history, app }) {
-
+    const routerData = getRouterData(app);
+    const UserLayout = routerData['/user'].component;
+    const BasicLayout = routerData['/'].component;
     return (
         <LocaleProvider locale={zhCN}>
             <ConnectedRouter history={history}>
                 <Switch>
-                    <Route exact path="/" render={() => (<Redirect to="/dashboard" />)} />
+                    <Route path="/user" component={UserLayout} />
+                    <AuthorizedRoute
+                        path="/"
+                        render={props => <BasicLayout {...props} />}
+                        authority={['admin', 'user']}
+                        redirectPath="/user/login"
+                    />
                 </Switch>
             </ConnectedRouter>
         </LocaleProvider>
