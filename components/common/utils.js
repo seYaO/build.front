d4c8b7cb5184a30df2070a611cc25f8de3dd0401
lily <wl09720@ly.com>
2018-11-09 11:09:51 +0800

    common

--- components/common/utils.js
+++ components/common/utils.js
@@ -1,10 +1,10 @@
 function isDef(value) {
-  return value !== undefined && value !== null;
+    return value !== undefined && value !== null;
 }
 
 function isObj(x) {
-  var type = typeof x;
-  return x !== null && (type === 'object' || type === 'function');
+    var type = typeof x;
+    return x !== null && (type === 'object' || type === 'function');
 }
 
 export { isObj, isDef };
