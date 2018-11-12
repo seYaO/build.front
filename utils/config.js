148e8df31b25a3bdc7bb61e5688b0c603c0f1c1f
lily <wl09720@ly.com>
2018-11-12 09:52:11 +0800

    update

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,13 @@
+function test(time){
+    var date = new Date();
+    var nowTime = new Date().getTime();
+    var first = `${date.getFullYear()}/${date.getMonth() + 1}/${data.getDate()} ${time}`
+    var firstTime = new Date(first).getTime();
+    var num = nowTime - firstTime;
+    num = num/1000/60/60;
+    return num;
+}
+
 module.exports = {
     // 登录类型-境外
     loginType: 'JWLYX',
