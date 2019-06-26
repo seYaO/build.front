2bdee651e0487ca824b1eb477472196bda018ce9
lily <wl09720@ly.com>
2019-06-26 17:13:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,10 @@ git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 git push -f git@github.com:seYaO/seYaO.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:seYaO/build.front.git master:gh-pages
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
