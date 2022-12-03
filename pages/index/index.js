931d29b931b53defa66a92552730dfb650fcaf8c
lily <wl09720@ly.com>
2022-12-03 13:09:40 +0800

    自考

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,13 @@
 import api from '../../services/beegoapi'
+import mock from '../../JSON/mock.js'
+
+const LIST = mock.list
+let CourseDetail = null
+LIST.map(item => {
+    if (item.courseDetail && item.productId == 24884128) {
+        CourseDetail = item.courseDetail
+    }
+})
 
 Page({
 
@@ -6,103 +15,130 @@ Page({
      * 页面的初始数据
      */
     data: {
-        token: '',
-        users: null
     },
-    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1ODY5NzMwIiwidWdpZCI6MSwiZXhwIjoxNjcwMzE0OTIxLCJ1c2VySWQiOjU4Njk3MzAsImlhdCI6MTY2OTcxMDEyMX0.0X56HisDStB8_XQq1hRSOxw5VE-Og3ev1eGJ1pRUJwem1f7LuqP7F1Z8dAFO1kcRHCsZSALm6X0JdT-58SOGdQ',
-    users: {
-        "district": null,
-        "adminId": 47232,
-        "token": null,
-        "lastLoginTime": "2022-11-29 16:20:54",
-        "userSetUp": null,
-        "verifyStatus": 1,
-        "isUpdateUserName": null,
-        "num": 1,
-        "tgIds": "",
-        "lastCourseId": "34-06090",
-        "lastUmcId": null,
-        "unsubscribe": 0,
-        "probationary": 0,
-        "mobileLoginCheck": null,
-        "wxMiniOpenId": null,
-        "wxNickName": null,
-        "follow": 0,
-        "lastUpdatePasswordTime": "2022-11-23T07:26:22.000+00:00",
-        "passwordErrorCount": 0,
-        "isAes": 0,
-        "aesPassword": null,
-        "realStatus": 0,
-        "realOff": 0,
-        "faceToken": null,
-        "productRealOff": 0,
-        "productId": "24881383,24881477,24881535,24881557,24884037,24884128,24884132,24884133,24884134,24884135,24884183",
-        "type": 22127,
-        "state": 0,
-        "valid": true,
-        "parentId": 0,
-        "userId": 5869730,
-        "agencyId": 9772,
-        "isValid": true,
-        "nickName": "lily",
-        "pic": "",
-        "mobile": "13140982991",
-        "userName": "LH13140982991",
-        "ugid": 1,
-        "mdomain": "szlhpx.beeeeego.com",
-        "password": "053c4314eb93716073b03d00d4583ddd",
-        "associated": false,
-        "regFrom": 13,
-        "remark": null,
-        "registerTime": "2022-11-23T07:26:23.000+00:00",
-        "sign": "",
-        "qq": "",
-        "email": "",
-        "openId": null,
-        "province": "江苏省",
-        "city": "苏州市",
-        "sex": "女",
-        "realName": "王莉",
-        "ticketNo": "",
-        "integration": 0,
-        "weChat": "",
-        "born": "",
-        "stuNo": "",
-        "machine": "",
-        "verifyCode": "",
-        "verifyTimes": 0,
-        "verifyTime": "2022-11-29 16:20:55",
-        "sign2": "",
-        "signChg": false,
-        "timeOut": 0,
-        "isAssociated": false,
-        "isSignChg": false
-    },
-
     async onLoad(options) {
-        // const user = await api.login()
-        // if (user.code == 200 && user.data) {
-        //     const { token, users } = user.data
-        //     this.setData({ token, users })
-        //     this.token = token
-        //     this.users = users
+        const token = wx.getStorageSync('token')
+        if (!token) {
+            const user = await api.login()
 
-        //     api.list(this.token)
+            if (!user) {
+                this.showModal()
+                return
+            }
 
-        //     this.getDetail(24884128)
+            wx.setStorageSync('token', user.token)
+        }
 
+        //获取所有课程信息
+        {
+            // let list = await api.list(wx.getStorageSync('token'))
 
+            // if (!list) {
+            //     this.showModal()
+            //     return
+            // }
 
-        // }
+            // list.map(async (item) => {
+            //     const courseID = await api.test({ token: wx.getStorageSync('token'), productId: item.productId })
+            //     item.courseId = courseID
+            //     const cstId = await api.detail({ token: wx.getStorageSync('token'), courseId: courseID, productId: item.productId })
+            //     item.cstId = cstId
+            //     item.chapterList = []
+            //     item.examList = []
+            // })
+            // console.log('list', list)
+            // setTimeout(() => {
+            //     console.log(JSON.stringify(list))
+            // }, 12000)
+        }
 
+        let idx = 10
 
-        api.list(this.token)
+        // 获取课程信息
+        {
+            // console.log('mock', LIST[idx])
+            // const item = LIST[idx]
+            // const chapter = await api.chapter({ token: wx.getStorageSync('token'), courseId: item.courseId, cstId: item.cstId })
+            // console.log('chapter', chapter)
+            // if (chapter) {
+            //     chapter.map(async (chapter) => {
+            //         chapter.children.map(async (child) => {
+            //             const exerList = await await api.practiceChapter({
+            //                 token: wx.getStorageSync('token'),
+            //                 courseId: item.courseId,
+            //                 cstId: item.cstId,
 
-        this.getDetail(24884128)
+            //                 catId: child.catId,
+            //                 linkName: child.linkName,
+            //                 name: child.name,
+            //             })
+            //             child.exerList = exerList
+            //         })
+            //     })
+                
+            //     setTimeout(() => {
+            //         console.log('chapter===================')
+            //         console.log('chapter===================')
+            //         console.log('chapter===================')
+            //         console.log(JSON.stringify(chapter))
+            //     }, 12000)
+            // }
+
+        }
+
+        // 获取模拟考试
+        {
+            console.log('mock', LIST[idx])
+            const item = LIST[idx]
+            const exam = await api.exam({ token: wx.getStorageSync('token'), courseId: item.courseId })
+            console.log('exam',exam)
+            if(exam){
+                exam.map(async (exam)=>{
+                    const exerList = await api.mockExam({ token: wx.getStorageSync('token'), courseId: item.courseId, cstId: item.cstId,specialProjectId:exam.specialProjectId })
+                    exam.exerList = exerList
+                })
+                setTimeout(() => {
+                    console.log('exam===================')
+                    console.log('exam===================')
+                    console.log('exam===================')
+                    console.log(JSON.stringify(exam))
+                }, 12000)
+            }
+        }
+    },
+
+    showModal() {
+        wx.showModal({
+            title: '提示',
+            content: '登录失败，是否重新登录？',
+            success: (res) => {
+                if (res.confirm) {
+                    wx.setStorageSync('token', '')
+                    this.onLoad()
+                    // console.log('用户点击确定')
+                } else if (res.cancel) {
+                    // console.log('用户点击取消')
+                }
+            }
+        })
+    },
+
+    async getList(list) {
+        list.map(async (item) => {
+            const courseID = await api.test({ token: wx.getStorageSync('token'), productId: item.productId })
+            item.courseId = courseID
+            const cstId = await api.detail({ token: wx.getStorageSync('token'), courseId: courseID, productId: item.productId })
+            item.cstId = cstId
+            // console.log('detail',detail)
+        })
+        console.log('list', list)
+        setTimeout(() => {
+            console.log(JSON.stringify(list))
+        }, 12000)
     },
 
     async getDetail(productId) {
-        const detail = await api.detail({ token: this.token, courseId: '34-06090', productId })
+        const detail = await api.detail({ token: wx.getStorageSync('token'), courseId: '34-06090', productId })
         if (detail.code == 200 && detail.data) {
             // console.log('detail',detail.data)
 
@@ -120,7 +156,7 @@ Page({
     },
 
     async getChapter(cstId) {
-        const chapter = await api.chapter({ token: this.token, courseId: '34-06090', cstId })
+        const chapter = await api.chapter({ token: wx.getStorageSync('token'), courseId: '34-06090', cstId })
         // console.log('chapter', chapter)
         if (chapter.code == 200 && chapter.data) {
             const practiceChapter = chapter.data.practiceChapter
@@ -139,7 +175,7 @@ Page({
 
             setTimeout(() => {
                 console.log(JSON.stringify(practiceChapter))
-            },12000)
+            }, 12000)
 
 
         }
@@ -147,7 +183,7 @@ Page({
 
     async getPractice(child) {
         const practice = await api.practiceChapter({
-            token: this.token,
+            token: wx.getStorageSync('token'),
             courseId: '34-06090',
             catId: child.catId,
             cstId: child.cstId,
--- services/beegoapi.js
+++ services/beegoapi.js
@@ -11,7 +11,12 @@ const login = () => {
                 'content-type': 'application/json' // 默认值
             },
             success(res) {
-                resolve(res.data)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    resolve(res.data.data)
+                    const info = res.data.data
+                } else {
+                    resolve(null)
+                }
             }
         })
     })
@@ -25,9 +30,47 @@ const list = (token = '') => {
             method: 'GET',
             header: { token },
             success(res) {
-                // console.log(res.data)
-                if (res.data && res.data.data && res.data.data.list) {
-                    // console.log('list', res.data.data.list)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data)
+                    const info = res.data.data
+                    let list = []
+                    info.list.map(item => {
+                        list.push({
+                            productId: item.productId,
+                            name: item.name,
+                            coverImages: item.coverImages,
+                            agencyId: item.agencyId,
+                        })
+                    })
+                    // console.log(JSON.stringify(list))
+                    resolve(list)
+                } else {
+                    resolve(null)
+                }
+            }
+        })
+    })
+}
+const test = (params) => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: `https://beegoapi.beeeeego.com/mobile/course/list`,
+            data: {
+                productId: params.productId,
+                pageNum: 1,
+                pageSize: 20,
+            },
+            method: 'POST',
+            header: {
+                token: params.token
+            },
+            success(res) {
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data.rows)
+                    const info = res.data.data.rows[0].courseID
+                    resolve(info)
+                } else {
+                    resolve(null)
                 }
             }
         })
@@ -44,9 +87,13 @@ const detail = (params) => {
                 token: params.token
             },
             success(res) {
-                resolve(res.data)
-                if (res.data && res.data.data) {
-                    // console.log('courseInformationList', res.data.data.courseInformationList)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data)
+                    const info = res.data.data.cstId
+                    resolve(res.data.data.cstId)
+                    console.log(info, 'info')
+                } else {
+                    resolve(null)
                 }
             }
         })
@@ -67,13 +114,121 @@ const chapter = (params) => {
                 token: params.token
             },
             success(res) {
-                resolve(res.data)
-                if (res.data && res.data.data) {
-                    // console.log('chapter', res.data.data)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data)
+                    const info = res.data.data
+                    let list = []
+                    info.practiceChapter.map(item => {
+                        let obj = {
+                            courseId: params.courseId,
+                            cstId: params.cstId,
+                            "id": item.id,
+                            "parentId": item.parentId,
+                            catId: item.catId,
+                            name: item.name,
+                            linkName: item.linkName,
+                            children: []
+                        }
+                        item.children.map(child => {
+                            obj.children.push({
+                                cstId: params.cstId,
+                                catId: child.catId,
+                                "id": child.id,
+                                "parentId": child.parentId,
+                                "name": child.name,
+                                "linkName": child.linkName,
+
+                                // title: child.title,
+                                // "a": child.a,
+                                // "b": child.b,
+                                // "c": child.c,
+                                // "d": child.d,
+                                // "e": child.e,
+                                // "f": child.f,
+                                // "g": child.g,
+                                // "h": child.h,
+                                // "rightKey": child.rightKey,
+                                // "keyType": child.keyType,
+                                // "analyze": child.analyze, // 简述答案
+                                // "matRef": child.matRef,
+                                // "exerType": child.exerType,
+                                // "source": child.source,
+                                // "rightKeyList": child.rightKeyList,
+                            })
+                        })
+                        list.push(obj)
+                    })
+                    // console.log('list', list)
+                    resolve(list)
+                } else {
+                    resolve(null)
                 }
             }
         })
     })
+}
+
+const sss = {
+    "simID": 11606,
+    "examName": "人员素质测评理论与方法模拟题二",
+    "courseID": "34-06090",
+    "examDate": "2010",
+    "totalScore": 100,
+    "passScore": 60,
+    "exerNum": 40,
+    "zone": "全国",
+    "simType": "模拟题",
+    "examTime": 150,
+    "memo": "                2020/8/7\r\n",
+    "isVaild": 1,
+    "getScoreCondition": null,
+    "sequence": 11606,
+    "scoringMethod": 1,
+    "simExamCount": null,
+    "startTime": null,
+    "endTime": null,
+    "authentication": 0,
+    "verifyCode": null,
+    "exerSortType": 1,
+    "createTime": null,
+    "agencyId": 0,
+    "topicOrder": null,
+    "isSelectExam": false,
+    "isSample": 0,
+    "sampleNum": null,
+    "srId": 8880221,
+    "typeName": null,
+    "mark": 0,
+    "isExamination": 0,
+    "catalogName": null,
+    "doRecordNum": 0,
+    "cstId": null,
+    "title": null,
+    "sampleNumJson": null,
+    "isFinish": null,
+    "isCollection": 0,
+    "score": 0,
+    "openStartDate": "",
+    "openEndDate": "",
+    "doTimes": 150,
+    "doNum": -1,
+    "sortType": 0,
+    "intro": null,
+    "courseName": null,
+    "lastPosition": 0,
+    "specialProjectId": 130012,
+    "lastExam": 0,
+    "isOpen": 1,
+    "projectName": "人员素质测评理论与方法模拟题二",
+    "myCollectionId": null,
+    "collectionType": null,
+    "isTrailers": 1,
+    "productId": 0,
+    "simRecordType": 2,
+    "expiredStatus": false,
+    "umcId": 30534611,
+    "prname": null,
+    "courseNo": null
 }
 
 // 每小节练习题
@@ -96,9 +251,34 @@ const practiceChapter = (params) => {
                 token: params.token
             },
             success(res) {
-                resolve(res.data)
                 if (res.data && res.data.data) {
-                    // console.log('practice', res.data.data)
+                    const info = res.data.data
+                    let list = []
+                    info.exerList.map(item => {
+                        let obj = {
+                            title: item.title,
+                            "a": item.a,
+                            "b": item.b,
+                            "c": item.c,
+                            "d": item.d,
+                            "e": item.e,
+                            "f": item.f,
+                            "g": item.g,
+                            "h": item.h,
+                            "rightKey": item.rightKey,
+                            "keyType": item.keyType,
+                            "analyze": item.analyze, // 简述答案
+                            "matRef": item.matRef,
+                            "exerType": item.exerType,
+                            "source": item.source,
+                            "rightKeyList": item.rightKeyList,
+                        }
+                        list.push(obj)
+                    })
+                    // console.log('list', list)
+                    resolve(list)
+                } else {
+                    resolve(null)
                 }
             }
         })
@@ -115,9 +295,23 @@ const exam = (params) => {
                 token: params.token
             },
             success(res) {
-                resolve(res.data)
-                if (res.data && res.data.data) {
-                    // console.log('courseInformationList', res.data.data.courseInformationList)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data)
+                    const info = res.data.data
+                    let list = []
+                    info.simExams.map(item => {
+                        let obj = {
+                            "examName": item.examName,
+                            "specialProjectId": item.specialProjectId,
+                            "projectName": item.projectName,
+                            "zone": item.zone,
+                            "simType": item.simType,
+                        }
+                        list.push(obj)
+                    })
+                    resolve(list)
+                } else {
+                    resolve(null)
                 }
             }
         })
@@ -128,15 +322,38 @@ const exam = (params) => {
 const mockExam = (params) => {
     return new Promise((resolve, reject) => {
         wx.request({
-            url: `https://beegoapi.beeeeego.com/mobile/mockExam/getMockExam?specialProjectId=130012&cstId=10393&courseId=34-06090&isRedo=1&simRecordType=2`,
+            url: `https://beegoapi.beeeeego.com/mobile/mockExam/getMockExam?specialProjectId=${params.specialProjectId}&cstId=${params.cstId}&courseId=${params.courseId}&isRedo=1&simRecordType=2`,
             method: 'GET',
             header: {
                 token: params.token
             },
             success(res) {
-                resolve(res.data)
-                if (res.data && res.data.data) {
-                    // console.log('courseInformationList', res.data.data.courseInformationList)
+                if (res.data && res.data.code == 200 && res.data.data) {
+                    // resolve(res.data.data)
+                    const info = res.data.data
+                    let list = []
+                    info.exerList.map(item => {
+                        let obj = {
+                            "source": item.source,
+                            "keyType": item.keyType,
+                            "exerType": item.exerType,
+                            "title": item.title,
+                            "analyze": item.analyze,
+                            "rightKey": item.rightKey,
+                            "a": item.a,
+                            "b": item.b,
+                            "c": item.c,
+                            "d": item.d,
+                            "e": item.e,
+                            "f": item.f,
+                            "g": item.g,
+                            "h": item.h,
+                        }
+                        list.push(obj)
+                    })
+                    resolve(list)
+                } else {
+                    resolve(null)
                 }
             }
         })
@@ -145,6 +362,7 @@ const mockExam = (params) => {
 
 module.exports = {
     login,
+    test,
     list,
     detail,
     chapter,
